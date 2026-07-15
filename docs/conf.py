4d243e1f4700a9636c86c4a970605b21387a74bd
taxguide <singhparash06@gmail.com>
2026-07-15 17:19:15 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,23 +4,17 @@ project = 'Ancestry Login Guide'
 author = 'Ancestry Guide'
 release = '1.0'
 
-# Extensions
 extensions = []
 
-# Templates
 templates_path = []
 
-# Exclude build files
 exclude_patterns = []
 
-# HTML Theme
 html_theme = 'alabaster'
 
 # Static files
 html_static_path = ['_static']
 
-# Language
 language = 'en'
 
-# Output settings
 html_title = "Ancestry.com Login Guide 2026"
