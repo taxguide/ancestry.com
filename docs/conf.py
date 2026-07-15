5477751f7e3da244d7bfc87c4baa0b224775a2d2
taxguide <singhparash06@gmail.com>
2026-07-15 16:53:35 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
 
+project = 'Ancestry Login Guide'
+author = 'Ancestry Guide'
+release = '1.0'
+
+
+# Extensions
+extensions = []
+
+
+# Templates
+templates_path = []
+
+
+# Exclude build files
+exclude_patterns = []
+
+
+# HTML Theme
+html_theme = 'alabaster'
+
+
+# Static files
+html_static_path = []
+
+
+# Language
+language = 'en'
+
+
+# Output settings
+html_title = "Ancestry.com Login Guide 2026"
