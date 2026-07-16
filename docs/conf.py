66ea2238b05e47b5e5c5e4b4aafc315e8385cf29
taxguide <singhparash06@gmail.com>
2026-07-16 15:11:26 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,18 +4,28 @@ project = 'Ancestry Login Guide'
 author = 'Ancestry Guide'
 release = '1.0'
 
-extensions = []
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
 
 # Templates
 templates_path = ['_templates']
 
 exclude_patterns = []
 
+# Theme
 html_theme = 'alabaster'
 
 # Static files
 html_static_path = ['_static']
 
+# Language
 language = 'en'
 
+# Browser Title
 html_title = "Ancestry.com Login Guide 2026"
+
+# Sitemap
+html_baseurl = "https://login-ancestrycom-login.readthedocs-hosted.com/en/latest/"
+sitemap_url_scheme = "{link}"
